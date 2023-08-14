43a18b4f4593376be25b8cb9a2dbbfd0e7b9c74f
Amy <amy@litdevs.org>
2023-08-14 20:48:17 +0300

    Initial upload endpoint

--- src/routes/v1/file.ts
+++ src/routes/v1/file.ts
@@ -3,13 +3,25 @@ import RequiredProperties from "../../middleware/RequiredProperties.js";
 import Reply from "../../classes/Reply/Reply.js";
 import ServerErrorReply from "../../classes/Reply/ServerErrorReply.js";
 import Database from "../../db.js";
-import {isValidObjectId} from "mongoose";
+import {isValidObjectId, Types} from "mongoose";
 import NotFoundReply from "../../classes/Reply/NotFoundReply.js";
-import {fileTypeFromStream, fileTypeStream} from "file-type";
+import {fileTypeStream} from "file-type";
 import {contentType} from "mime-types";
+import multer from "multer";
+import fs from "fs";
+import * as crypto from "crypto";
+import {isArray} from "util";
+import BadRequestReply from "../../classes/Reply/BadRequestReply.js";
 const router = express.Router();
 
 const database = new Database();
+
+const tmpStorage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, crypto.randomBytes(8).toString("base64url"))
+    }
+})
+const upload = multer({storage: tmpStorage})
 
 router.get("/:fileId", async (req, res) => {
     const download = !!req.query?.download
@@ -39,7 +51,42 @@ router.get("/:fileId", async (req, res) => {
 
     //console.log(typeIdStream.length)
     fileStream.pipe(res);
-
 })
+
+
+const uploadEndpoint = async (req, res) => {
+    // TODO: Auth
+    // TODO: Private and persistent files (depends on auth)
+    if (!req.files || !Array.isArray(req.files) || req.files.length < 1) return res.reply(new BadRequestReply("Upload at least one file in multipart/form-data request"))
+    let uploadPromises : Promise<string>[] = req.files.map(file => new Promise(resolve => {
+        let readStream = fs.createReadStream(file.path);
+        let shortId = crypto.randomBytes(4).toString("base64url"); // (2^8)^4 = 4294967296
+        let uploadStream = database.FileBucket.openUploadStream(file.originalname, {
+            metadata: {
+                shortId,
+                uploadedBy: new Types.ObjectId(0),
+                private: false,
+                persistent: false
+            }
+        });
+        readStream.pipe(uploadStream);
+        uploadStream.on('finish', () => {
+            resolve(shortId);
+        });
+    }))
+
+    let uploads : string[] = await Promise.all(uploadPromises);
+    res.reply(new Reply({
+        response: {
+            message: "Upload complete",
+            uploadedFiles: uploads.map(u => `http://localhost:45303/v1/file/${u}`)
+        }
+    }))
+}
+
+// This really should be a PUT imo but backwards compatibility, so it will be a POST
+router.post("/upload", upload.any(), uploadEndpoint)
+router.put("/", upload.any(), uploadEndpoint)
+
 
 export default router;
